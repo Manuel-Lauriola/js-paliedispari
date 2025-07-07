5e8e556b780e49574b7c84db1b6696d74d9bc57b
Manuel-Lauriola <manuelauriol@gmail.com>
2025-07-07 18:03:12 +0200

    [UPD] - function now works as intended

--- pari-e-dispari/script.js
+++ pari-e-dispari/script.js
@@ -1,11 +1,12 @@
 //Chiedo all'utente di scegliere pari o dispari
 const oddOrEven = prompt(`digita pari o dispari`)
+console.log(`hai scelto ${oddOrEven}`)
 //e il numero da giocare
 const userNumber = parseInt(prompt(`inserisci un numero tra 1 e 5`))
-// console.log(userNumber)
+console.log(`hai inserito ${userNumber}`)
 //genero il pcNumber
 const pcNumber = Math.floor(Math.random()*5+ 1)
-// console.log(pcNumber)
+console.log(`il computer ha generato ${pcNumber}`)
 //definisco una funzione con parametri pari o dispari, il numero dell'utente e quello del pc
 function oddEvenGame (evenOrOdd, x, y) {
   //eseguo la somma tra userNumber e pcNumber
@@ -19,6 +20,13 @@ function oddEvenGame (evenOrOdd, x, y) {
       console.log(`${userNumber} + ${pcNumber} = ${sum} è dispari`)
       sum = `dispari`
     }
+    //dopo aver dato un valore in stringa a sum lo confronto con oddOrEven inserito dall'utente e dichiaro il vincitore
+    if (sum == oddOrEven) {
+      console.log(`Hai vinto`)
+    }
+      else {
+        console.log(`Hai perso`)
+      }
     
 }
 
