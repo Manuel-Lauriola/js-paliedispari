a0f9e296e5b35f1fa444d358110e9aa25f0b36e7
Manuel-Lauriola <manuelauriol@gmail.com>
2025-07-07 16:43:07 +0200

    [UPD]-function palindrome setup

--- palindroma/script.js
+++ palindroma/script.js
@@ -1,2 +1,12 @@
-//chiedo all'utente di inserire una parola e la salvo nella variabile word
-const word = prompt(`Inserisci una parola e verificherò se essa è palindroma per te`)
+//chiedo all'utente di inserire una parola e la salvo nella variabile userWord
+const userWord = prompt(`Inserisci una parola e verificherò se essa è palindroma per te`)
+//definisco la funzione che mi verificherà se la parola è palindroma
+//definisco una funzione con parametro word
+function palindrome (word) {
+  //definisco l'array charachters che conterrà i caratteri di word
+  let characters = word.split("")
+  //applico la funzione .reverse
+  let reversedWord = characters.reverse()
+  //unisco l'array generato in una stringa
+  reversedWord = reversedWord.join("")
+}
