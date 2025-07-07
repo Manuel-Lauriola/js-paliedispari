b72a73fc91acaae464dec7248ae292bb737e241e
Manuel-Lauriola <manuelauriol@gmail.com>
2025-07-07 17:13:25 +0200

    [UPD] - prompt oddOrEven and userNumber

---
+++ pari-e-dispari/script.js
@@ -0,0 +1,5 @@
+//Chiedo all'utente di scegliere pari o dispari
+const oddOrEven = prompt(`digita pari o dispari`)
+//e il numero da giocare
+const userNumber = prompt(`inserisci un numero tra 1 e 5`)
+console.log(oddOrEven, userNumber)
